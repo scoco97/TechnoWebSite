c47596d50138b2197b3994c187143d9280c6391a
scoco97 <yashjain864@gmail.com>
2017-09-06 17:36:52 +0530

    First-commit

--- functions/eventsRegister.js
+++ functions/eventsRegister.js
@@ -2,43 +2,6 @@ const app = require('../express')
 const con = require('../db');
 
 app.post('/register/javaguru', function(req,res){
-  var email = req.body.email;
-  var password = req.body.password;
-  connection.query('SELECT * FROM users WHERE email = ?',[email], function (error, results, fields) {
-  if (error) {
-    console.log("error ocurred",error);
-    res.send({
-      "code":400,
-      "failed":"error ocurred"
-    })
-  }
-  else
-  {
-    // console.log('The solution is: ', results);
-    if(results.length > 0)
-    {
-      bcrypt.compare(password, results[0].password, function(err, doesMatch){
-      if(doesMatch){
-        res.send({
-          "code":200,
-          "success":"login sucessfull"
-            });
-      }
-      else{
-        res.send({
-          "code":204,
-          "success":"Email and password does not match"
-            });
-      }
-    });
-  }
-    else{
-      res.send({
-        "code":204,
-        "success":"Email does not exits"
-          });
-    }
-}
-});
+
 }
 );
