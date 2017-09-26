4ac5a370194a728c770b4d1dfd6032cbbe385b5d
Siddhesh Rane <siddheshrane24@gmail.com>
2017-09-26 18:26:55 +0530

    added registration js

--- app.js
+++ app.js
@@ -21,7 +21,15 @@ app.get('/aqua-register',(req,res)=>{
     res.sendFile(path.join(__dirname + '/public/register/aqua-register.html'));
 });
 app.post('/aqua-register',(req,res)=>{
-    
+  let leadername = req.body.leader_fullname;
+  let leaderemail = req.body.email;
+  let leadercontact = req.body.contact;
+  let member2name = req.body.member_2;
+  let member3name = req.body.member_3;
+  let member4name = req.body.member_4;
+  let collegename = req.body.collegeName;
+  let house = req.body.House;
+  console.log(req.body); 
 });
 
 
@@ -31,6 +39,14 @@ app.get('/climb-e-rope-register',(req,res)=>{
     res.sendFile(path.join(__dirname + '/public/register/climb-e-rope.html'));
 });
 app.post('/climb-e-rope-register',(req,res)=>{
+   let leadername = req.body.leader_fullname;
+  let leaderemail = req.body.email;
+  let leadercontact = req.body.contact;
+  let member2name = req.body.member_2;
+  let member3name = req.body.member_3;
+  let collegename = req.body.collegeName;
+  let house = req.body.House;
+  console.log(req.body); 
 });
 
 
@@ -41,7 +57,12 @@ app.get('/codeinx-register',(req,res)=>{
     res.sendFile(path.join(__dirname + '/public/register/codeinx-register.html'));
 });
 app.post('/codeinx-register',(req,res)=>{
-
+ let name = req.body.fullname;
+  let email = req.body.email;
+  let contact = req.body.contact;
+  let collegeName = req.body.collegeName;
+  let house = req.body.House;
+  console.log(req.body);
 });
 
 
@@ -51,7 +72,12 @@ app.get('/coderoyale-register',(req,res)=>{
     res.sendFile(path.join(__dirname + '/public/register/coderoyale-register.html'));
 });
 app.post('/coderoyale-register',(req,res)=>{
-
+ let name = req.body.fullname;
+  let email = req.body.email;
+  let contact = req.body.contact;
+  let collegeName = req.body.collegeName;
+  let house = req.body.House;
+  console.log(req.body);
 });
 
 
@@ -60,6 +86,15 @@ app.get('/codeswap-register',(req,res)=>{
     res.sendFile(path.join(__dirname + '/public/register/codeswap-register.html'));
 });
 app.post('/codeswap-register',(req,res)=>{
+  let leadername = req.body.member_1;
+  let leaderemail = req.body.email_1;
+  let leadercontact = req.body.contact_1;
+  let member2name = req.body.member_2;
+  let member3name = req.body.email_2;
+  let member4name = req.body.contact_2;
+  let collegename = req.body.collegeName;
+  let house = req.body.House;
+  console.log(req.body); 
 
 });
 
@@ -69,7 +104,12 @@ app.get('/cryptext-register',(req,res)=>{
     res.sendFile(path.join(__dirname + '/public/register/cryptext-register.html'));
 });
 app.post('/cryptext-register',(req,res)=>{
-
+ let name = req.body.fullname;
+  let email = req.body.email;
+  let contact = req.body.contact;
+  let collegeName = req.body.collegeName;
+  let house = req.body.House;
+  console.log(req.body);
 });
 
 
@@ -78,6 +118,12 @@ app.get('/cway-register',(req,res)=>{
     res.sendFile(path.join(__dirname + '/public/register/cway-register.html'));
 });
 app.post('/cway-register',(req,res)=>{
+   let name = req.body.fullname;
+  let email = req.body.email;
+  let contact = req.body.contact;
+  let collegeName = req.body.collegeName;
+  let house = req.body.House;
+  console.log(req.body);
 });
 
 
@@ -87,8 +133,32 @@ app.get('/ic-register',(req,res)=>{
     res.sendFile(path.join(__dirname + '/public/register/ic-register.html'));
 });
 app.post('/ic-register',(req,res)=>{
+   let leadername = req.body.leader_fullname;
+  let leaderemail = req.body.email;
+  let leadercontact = req.body.contact;
+  let member2name = req.body.member_2;
+  let member3name = req.body.member_3;
+  let member4name = req.body.member_4;
+  let member5name = req.body.member_5;
+  let member6name = req.body.member_6;
+  let collegename = req.body.collegeName;
+  let house = req.body.House;
+  console.log(req.body);
 });
 
+
+//bdg
+app.get('/bridgethegap-register',(req,res)=>{
+    res.sendFile(path.join(__dirname + '/public/register/bridgethegap-register.html'));
+});
+app.post('/bridgethegap-register',(req,res)=>{
+ let name = req.body.fullname;
+  let email = req.body.email;
+  let contact = req.body.contact;
+  let collegeName = req.body.collegeName;
+  let house = req.body.House;
+  console.log(req.body);
+});
 
 
 //iot
@@ -96,7 +166,12 @@ app.get('/iot-register',(req,res)=>{
     res.sendFile(path.join(__dirname + '/public/register/iot-register.html'));
 });
 app.post('/iot-register',(req,res)=>{
-
+ let name = req.body.fullname;
+  let email = req.body.email;
+  let contact = req.body.contact;
+  let collegeName = req.body.collegeName;
+  let house = req.body.House;
+  console.log(req.body);
 });
 
 
@@ -106,6 +181,12 @@ app.get('/javaguru-register',(req,res)=>{
     res.sendFile(path.join(__dirname + '/public/register/javaguru-register.html'));
 });
 app.post('/javaguru-register',(req,res)=>{
+   let name = req.body.fullname;
+  let email = req.body.email;
+  let contact = req.body.contact;
+  let collegeName = req.body.collegeName;
+  let house = req.body.House;
+  console.log(req.body);
 });
 
 
@@ -115,6 +196,15 @@ app.get('/maze-register',(req,res)=>{
     res.sendFile(path.join(__dirname + '/public/register/maze-register.html'));
 });
 app.post('/maze-register',(req,res)=>{
+  let leadername = req.body.leader_fullname;
+  let leaderemail = req.body.email;
+  let leadercontact = req.body.contact;
+  let member2name = req.body.member_2;
+  let member3name = req.body.member_3;
+  let member4name = req.body.member_4;
+  let collegename = req.body.collegeName;
+  let house = req.body.House;
+  console.log(req.body); 
 });
 
 
@@ -123,6 +213,26 @@ app.get('/missionsql-register',(req,res)=>{
     res.sendFile(path.join(__dirname + '/public/register/missionsql-register.html'));
 });
 app.post('/missionsql-register',(req,res)=>{
+   let name = req.body.fullname;
+  let email = req.body.email;
+  let contact = req.body.contact;
+  let collegeName = req.body.collegeName;
+  let house = req.body.House;
+  console.log(req.body);
+});
+
+
+//rcmo
+app.get('/rcmo-register',(req,res)=>{
+    res.sendFile(path.join(__dirname + '/public/register/rcmo-register.html'));
+});
+app.post('/rcmo-register',(req,res)=>{
+   let name = req.body.fullname;
+  let email = req.body.email;
+  let contact = req.body.contact;
+  let collegeName = req.body.collegeName;
+  let house = req.body.House;
+  console.log(req.body);
 });
 
 
@@ -131,6 +241,15 @@ app.get('/monsterarena-register',(req,res)=>{
     res.sendFile(path.join(__dirname + '/public/register/monsterarena-register.html'));
 });
 app.post('/monsterarena-register',(req,res)=>{
+  let leadername = req.body.leader_fullname;
+  let leaderemail = req.body.email;
+  let leadercontact = req.body.contact;
+  let member2name = req.body.member_2;
+  let member3name = req.body.member_3;
+  let member4name = req.body.member_4;
+  let collegename = req.body.collegeName;
+  let house = req.body.House;
+  console.log(req.body); 
 });
 
 
@@ -140,6 +259,12 @@ app.get('/myst-register',(req,res)=>{
     res.sendFile(path.join(__dirname + '/public/register/myst-register.html'));
 });
 app.post('/myst-register',(req,res)=>{
+   let name = req.body.fullname;
+  let email = req.body.email;
+  let contact = req.body.contact;
+  let collegeName = req.body.collegeName;
+  let house = req.body.House;
+  console.log(req.body);
 });
 
 
@@ -148,6 +273,15 @@ app.get('/robosoccer-register',(req,res)=>{
     res.sendFile(path.join(__dirname + '/public/register/robosoccer-register.html'));
 });
 app.post('/robosoccer-register',(req,res)=>{
+  let leadername = req.body.leader_fullname;
+  let leaderemail = req.body.email;
+  let leadercontact = req.body.contact;
+  let member2name = req.body.member_2;
+  let member3name = req.body.member_3;
+  let member4name = req.body.member_4;
+  let collegename = req.body.collegeName;
+  let house = req.body.House;
+  console.log(req.body); 
 });
 
 
@@ -155,14 +289,59 @@ app.post('/robosoccer-register',(req,res)=>{
 app.get('/robosumo-register',(req,res)=>{
     res.sendFile(path.join(__dirname + '/public/register/robosumo-register.html'));
 });
-app.get('/robosumo-register',(req,res)=>{
+app.post('/robosumo-register',(req,res)=>{
+  let leadername = req.body.leader_fullname;
+  let leaderemail = req.body.email;
+  let leadercontact = req.body.contact;
+  let member2name = req.body.member_2;
+  let member3name = req.body.member_3;
+  let member4name = req.body.member_4;
+  let member5name = req.body.member_5;
+  let member6name = req.body.member_6;
+  let collegename = req.body.collegeName;
+  let house = req.body.House;
+  console.log(req.body);
+
 });
+
+
+
+//makerssquare
+app.get('makerssquare-register',(req,res)=>{
+    res.sendFile(path.join(__dirname + '/public/register/robosumo-register.html'));
+});
+app.post('/makerssquare-register',(req,res)=>{
+  let leadername = req.body.leader_fullname;
+  let leaderemail = req.body.email;
+  let leadercontact = req.body.contact;
+  let member2name = req.body.member_2;
+  let member3name = req.body.member_3;
+  let member4name = req.body.member_4;
+  let member5name = req.body.member_5;
+  let member6name = req.body.member_6;
+  let collegename = req.body.collegeName;
+  let house = req.body.House;
+  console.log(req.body);
+
+});
+
 
 //robomaze
 app.get('/robomaze-register',(req,res)=>{
     res.sendFile(path.join(__dirname + '/public/register/robomaze-register.html'));
 });
 app.get('/robomaze-register',(req,res)=>{
+let leadername = req.body.leader_fullname;
+  let leaderemail = req.body.email;
+  let leadercontact = req.body.contact;
+  let member2name = req.body.member_2;
+  let member3name = req.body.member_3;
+  let member4name = req.body.member_4;
+  let member5name = req.body.member_5;
+  let member6name = req.body.member_6;
+  let collegename = req.body.collegeName;
+  let house = req.body.House;
+  console.log(req.body);  
 });
 
 //drone
@@ -170,6 +349,37 @@ app.get('/drone-register',(req,res)=>{
     res.sendFile(path.join(__dirname + '/public/register/drone-register.html'));
 });
 app.get('/drone-register',(req,res)=>{
+    let leadername = req.body.leader_fullname;
+  let leaderemail = req.body.email;
+  let leadercontact = req.body.contact;
+  let member2name = req.body.member_2;
+  let member3name = req.body.member_3;
+  let member4name = req.body.member_4;
+  let member5name = req.body.member_5;
+  let member6name = req.body.member_6;
+  let collegename = req.body.collegeName;
+  let house = req.body.House;
+    console.log(req.body);
+});
+
+
+
+//fastnfurious
+app.get('/fastnfurious-register',(req,res)=>{
+    res.sendFile(path.join(__dirname + '/public/register/fastnfurious-register.html'));
+});
+app.get('/fastnfurious-register',(req,res)=>{
+    let leadername = req.body.leader_fullname;
+  let leaderemail = req.body.email;
+  let leadercontact = req.body.contact;
+  let member2name = req.body.member_2;
+  let member3name = req.body.member_3;
+  let member4name = req.body.member_4;
+  let member5name = req.body.member_5;
+  let member6name = req.body.member_6;
+  let collegename = req.body.collegeName;
+  let house = req.body.House;
+    console.log(req.body);
 });
 
 
@@ -179,15 +389,46 @@ app.get('/robowars-register',(req,res)=>{
     res.sendFile(path.join(__dirname + '/public/register/robowars-register.html'));
 });
 app.post('/robowars-register',(req,res)=>{
+   let leadername = req.body.leader_fullname;
+  let leaderemail = req.body.email;
+  let leadercontact = req.body.contact;
+  let member2name = req.body.member_2;
+  let member3name = req.body.member_3;
+  let member4name = req.body.member_4;
+  let member5name = req.body.member_5;
+  let member6name = req.body.member_6;
+  let collegename = req.body.collegeName;
+  let house = req.body.House;
+    console.log(req.body);
+
 });
 
+
+//tpp
+app.get('/tpp-register',(req,res)=>{
+    res.sendFile(path.join(__dirname + '/public/register/tpp-register.html'));
+});
+app.post('/tpp-register',(req,res)=>{
+  let name = req.body.fullname;
+  let email = req.body.email;
+  let contact = req.body.contact;
+  let collegeName = req.body.collegeName;
+  let house = req.body.House;
+  console.log(req.body);
+});
 
 
 //sherlock
 app.get('/sherlocked-register',(req,res)=>{
     res.sendFile(path.join(__dirname + '/public/register/sherlocked-register.html'));
 });
-app.post('/robowars-register',(req,res)=>{
+app.post('/sherlocked-register',(req,res)=>{
+  let name = req.body.fullname;
+  let email = req.body.email;
+  let contact = req.body.contact;
+  let collegeName = req.body.collegeName;
+  let house = req.body.House;
+  console.log(req.body);
 });
 
 
@@ -197,6 +438,15 @@ app.get('/smartcity-register',(req,res)=>{
     res.sendFile(path.join(__dirname + '/public/register/smartcity-register.html'));
 });
 app.get('/smartcity-register',(req,res)=>{
+  let leadername = req.body.leader_fullname;
+  let leaderemail = req.body.email;
+  let leadercontact = req.body.contact;
+  let member2name = req.body.member_2;
+  let member3name = req.body.member_3;
+  let member4name = req.body.member_4;
+  let collegename = req.body.collegeName;
+  let house = req.body.House;
+  console.log(req.body); 
 
 });
 
@@ -207,6 +457,15 @@ app.get('/technohunt-register',(req,res)=>{
     res.sendFile(path.join(__dirname + '/public/register/technohunt-register.html'));
 });
 app.post('/technohunt-register',(req,res)=>{
+let leadername = req.body.member_1;
+  let leaderemail = req.body.email_1;
+  let leadercontact = req.body.contact_1;
+  let member2name = req.body.member_2;
+  let member3name = req.body.email_2;
+  let member4name = req.body.contact_2;
+  let collegename = req.body.collegeName;
+  let house = req.body.House;
+  console.log(req.body);  
 });
 
 
@@ -216,6 +475,12 @@ app.get('/ultimatecoder-register',(req,res)=>{
     res.sendFile(path.join(__dirname + '/public/register/ultimatecoder-register.html'));
 });
 app.post('/ultimatecoder-register',(req,res)=>{
+   let name = req.body.fullname;
+  let email = req.body.email;
+  let contact = req.body.contact;
+  let collegeName = req.body.collegeName;
+  let house = req.body.House;
+  console.log(req.body);
 });
 
 
@@ -224,7 +489,19 @@ app.get('/vrc-register',(req,res)=>{
     res.sendFile(path.join(__dirname + '/public/register/vrc-register.html'));
 });
 app.post('/vrc-register',(req,res)=>{
-  console.log(req);
+
+  let leadername = req.body.leader_fullname;
+  let leaderemail = req.body.email;
+  let leadercontact = req.body.contact;
+  let member2name = req.body.member_2;
+  let member3name = req.body.member_3;
+  let member4name = req.body.member_4;
+  let member5name = req.body.member_5;
+  let member6name = req.body.member_6;
+  let collegename = req.body.collegeName;
+  let house = req.body.House;
+    console.log(req.body);
+
 });
 
 
@@ -233,6 +510,12 @@ app.get('/vsm-register',(req,res)=>{
     res.sendFile(path.join(__dirname + '/public/register/vsm-register.html'));
 });
 app.post('/vsm-register',(req,res)=>{
+   let name = req.body.fullname;
+  let email = req.body.email;
+  let contact = req.body.contact;
+  let collegeName = req.body.collegeName;
+  let house = req.body.House;
+  console.log(req.body);
 });
 
 
