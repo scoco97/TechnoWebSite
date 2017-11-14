60bb4a9f2c3a821ffe7bf458ab6dbe3732b687ba
scoco97 <yashjain864@gmail.com>
2017-11-14 22:11:18 +0530

    Added TPP/MakersSquare Drafs

--- app.js
+++ app.js
@@ -1434,6 +1434,14 @@ app.get('/SmartCity_DRAFT_1',function(req,res){
   res.sendFile(path.join(__dirname + '/public/downloads/SmartCity 2017_Draft-1.pdf'));
 });
 
+app.get('/MakersSquare_DRAFT_1',function(req,res){
+  res.sendFile(path.join(__dirname + '/public/downloads/Makers Square 2017_Draft-1.pdf'));
+});
+
+app.get('/TPP_DRAFT_1',function(req,res){
+  res.sendFile(path.join(__dirname + '/public/downloads/TPP 2017_Draft-1.pdf'));
+});
+
 app.listen(process.env.PORT||3000, function() {
   console.log('Example app listening on port 3000!')
 })
